0b2c69f600202aa5f22bc9764b4af3995caeba70
sabbir <sabbir@gmail.com>
2023-07-01 17:25:37 +0600

    contact section done

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import "./Contact.css";
 import Button from "../components/Button/Button";
+import { Slide } from "react-awesome-reveal";
 
 const contactInfo = [
   {
@@ -20,52 +21,62 @@ const contactInfo = [
 const Contact: FC = () => {
   return (
     <section>
-      <h2 className="section__title">Get in touch</h2>
-      <p className="section__subtitle"> contact me </p>
+      <Slide direction="left">
+        <h2 className="section__title">Get in touch</h2>
+      </Slide>
+      <Slide direction="right">
+        <p className="section__subtitle"> contact me </p>
+      </Slide>
 
       <div className="contact_container">
-        <div className="contact_content">
-          <div className="contact_title">
-            <h2>Talk to me</h2>
+        <Slide direction="left">
+          {" "}
+          <div className="contact_content">
+            <div className="contact_title">
+              <h2>Talk to me</h2>
+            </div>
+
+            <div className="contact_info">
+              {contactInfo.map((info, ind) => {
+                return (
+                  <div key={ind} className="contact_card">
+                    <h3>{info.name}</h3>
+                    <p className="card_title">{info.email}</p>
+                    <p>write me --</p>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="contact_info">
-            {contactInfo.map((info, ind) => {
-              return (
-                <div key={ind} className="contact_card">
-                  <h3>{info.name}</h3>
-                  <p className="card_title">{info.email}</p>
-                  <p>write me --</p>
-                </div>
-              );
-            })}
+        </Slide>
+
+        <Slide direction="right">
+          <div className="contact_form">
+            <div className="contact_title">
+              <h2>Talk to me</h2>
+            </div>
+
+            <form action="">
+              <input
+                className="contact_form_input"
+                type="text"
+                placeholder="your name"
+              />
+              <input
+                className="contact_form_input"
+                type="email"
+                placeholder="your email"
+              />
+
+              <textarea
+                className="contact_form_input"
+                placeholder="write something"
+              ></textarea>
+
+              <Button />
+            </form>
           </div>
-        </div>
-
-        <div className="contact_form">
-          <div className="contact_title">
-            <h2>Talk to me</h2>
-          </div>
-
-          <form action="">
-            <input
-              className="contact_form_input"
-              type="text"
-              placeholder="your name"
-            />
-            <input
-              className="contact_form_input"
-              type="email"
-              placeholder="your email"
-            />
-
-            <textarea
-              className="contact_form_input"
-              placeholder="write something"
-            ></textarea>
-
-            <Button />
-          </form>
-        </div>
+        </Slide>
       </div>
     </section>
   );
