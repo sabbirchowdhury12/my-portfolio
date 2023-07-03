02f5485b62bb0066d068efb9976d53d2c0e1f7dd
sabbir <sabbir@gmail.com>
2023-07-03 16:36:43 +0600

    project section done

--- src/Project/Project.tsx
+++ src/Project/Project.tsx
@@ -5,40 +5,49 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "./Project.css";
 
+import img1 from "../assets/phone.png";
+import img2 from "../assets/tour.png";
+import img3 from "../assets/chat.png";
+
 export const data = [
   {
     id: 1,
-    title: "Chat GPT-3 AI",
-    img_url: "",
-    link: "https://chat-gpt-clone-gilt.vercel.app/",
-    gitHub: "https://github.com/tijanisylla/chat-gpt",
+    title: "Resale Phone",
+    img_url: img1,
+    client:
+      "https://github.com/sabbirchowdhury12/sabbirchowdhury12-mern-stack-repair-resale-phone-client",
+    server:
+      "https://github.com/sabbirchowdhury12/mern-stack-Repair-Resale-Phone-server",
+    live: "https://resale-repair-phone.web.app/",
+    name: "Resale and Buy Phone",
   },
   {
     id: 2,
-    title: "Movie App",
-    img_url: "",
-    link: "https://tijanisylla.github.io/movie-app",
-    gitHub: "https://github.com/tijanisylla/movie-app",
+    title: "Tour Website",
+    img_url: img2,
+    client:
+      "https://github.com/sabbirchowdhury12/mern-stack-tourist-service-website-client",
+    server:
+      "https://github.com/sabbirchowdhury12/mern-stack-tourist-service-website-server",
+    live: "https://world-tourist-website.web.app/",
+    name: "Tour Website",
   },
 
   {
     id: 3,
-    title: "Calculator",
-    img_url: "",
-    link: "https://calculator-sylla.netlify.app",
-    gitHub: "https://github.com/tijanisylla",
-  },
-  {
-    id: 4,
-    title: "Snake",
-    img_url: "",
-    link: "https://snake-sylla.netlify.app",
-    gitHub: "https://github.com/tijanisylla",
+    title: "Chat App",
+    img_url: img3,
+    client:
+      "https://github.com/sabbirchowdhury12/-mern-stack-chat-application-client-side",
+    server:
+      "https://github.com/sabbirchowdhury12/mern-stack-chat-application-server-side",
+    live: "https://mern-stack-chat-app.netlify.app/profile",
+    name: "Chat Application",
   },
 ];
 const Project = () => {
   return (
-    <section className="projects " id="projects">
+    <section className="projects container" id="projects">
       <Slide direction="left" triggerOnce>
         <h2 className="section__title">Projects</h2>
       </Slide>
@@ -56,35 +65,46 @@ const Project = () => {
               slidesPerView: 2,
               spaceBetween: 48,
             },
-            992: {
-              slidesPerView: 3,
-              spaceBetween: 48,
-            },
           }}
           modules={[Pagination]}
         >
-          {data.map(({ id, title, img_url, link }) => {
+          {data.map(({ id, title, img_url, live, name, client, server }) => {
             return (
               <SwiperSlide className="projects_card" key={id}>
                 <h3 className="projects_name">{title}</h3>
-                <img src={img_url} alt="img" className="projects_img" />
+                <div className=" projects_img">
+                  <img src={img_url} alt="img" />
+                </div>
 
                 <div>
+                  <div className="project_info">
+                    <p className="project_info_name">{name} (Full-Stack)</p>
+                  </div>
                   <a
-                    href={`${link}`}
+                    href={`${client}`}
+                    className="projects_social_icon"
+                    target="_blank"
+                    rel="noreferrer"
+                    title="client side"
+                  >
+                    <i className="fa-brands fa-github"></i>
+                  </a>
+                  <a
+                    href={`${server}`}
+                    className="projects_social_icon"
+                    target="_blank"
+                    rel="noreferrer"
+                    title="server side"
+                  >
+                    <i className="fa-brands fa-github"></i>
+                  </a>
+                  <a
+                    href={`${live}`}
                     className="projects_social_icon"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <i className="uil uil-globe"></i>
-                  </a>
-                  <a
-                    href="https://github.com/tijanisylla"
-                    className="projects_social-icon"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="uil uil-github-alt"></i>
+                    <i className="fa-sharp fa-solid fa-up-right-from-square"></i>
                   </a>
                 </div>
               </SwiperSlide>
--- src/components/Home/HomeSection/Social.tsx
+++ src/components/Home/HomeSection/Social.tsx
@@ -1,15 +1,38 @@
-import { BsFacebook, BsLinkedin , BsGithub} from 'react-icons/bs';
-import './Home.css'
-import { Fade } from 'react-awesome-reveal';
+import { BsFacebook, BsLinkedin, BsGithub } from "react-icons/bs";
+import "./Home.css";
+import { Fade } from "react-awesome-reveal";
 
 const Social = () => {
-    return (
-        <div className='social-icon'>
-            <Fade direction='up' triggerOnce delay={100}> <a href="" > <BsFacebook/>  </a></Fade> 
-            <Fade direction='up' triggerOnce delay={200}><a href="">  <BsLinkedin/></a></Fade> 
-            <Fade direction='up' triggerOnce delay={300}> <a href=""> <BsGithub/></a></Fade> 
-        </div>
-    );
+  return (
+    <div className="social-icon">
+      <Fade direction="up" triggerOnce delay={100}>
+        {" "}
+        <a
+          target="_blank"
+          href="https://www.facebook.com/profile.php?id=100075403486886"
+        >
+          {" "}
+          <BsFacebook />{" "}
+        </a>
+      </Fade>
+      <Fade direction="up" triggerOnce delay={200}>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/sabbir-chowdhury-891509257/"
+        >
+          {" "}
+          <BsLinkedin />
+        </a>
+      </Fade>
+      <Fade direction="up" triggerOnce delay={300}>
+        {" "}
+        <a target="_blank" href="https://github.com/sabbirchowdhury12">
+          {" "}
+          <BsGithub />
+        </a>
+      </Fade>
+    </div>
+  );
 };
 
 export default Social;
